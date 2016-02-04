077717a30144103da33c7d5f4c7176340cf04fc7
rana <Rana@GALILEO.contrado.local>
2016-02-04 16:53:01 +0000

    dasd

---
+++ BeeGame/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using System.Web.Mvc;
+using BeeGame.ServiceLayer.Builders;
+
+namespace BeeGame.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly BeeBuilder BeeBuilder = new BeeBuilder();
+
+        public ActionResult Index()
+        {
+            var model = BeeBuilder.Build();
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult Hit()
+        {
+            var data = BeeBuilder.Hit();
+
+            return new JsonResult { JsonRequestBehavior = JsonRequestBehavior.AllowGet, Data = data};
+        }
+    }
+}
---
+++ BeeGame/Scripts/beeIndex.js
@@ -0,0 +1,57 @@
+﻿$(function () {
+
+    var resetBeesColor = function () {
+        var $bees = $('.bee');
+        $.map($bees, function (element) {
+            $(element).css('background-color', 'black');
+        });
+    }
+
+    var onFinish = function () {
+        if (confirm('Game over, Start another round?')) {
+            location.reload();
+        }
+    }
+
+    var onBeeDead = function(id) {
+        var newId = 0;
+
+        var $selectedBee = $('.bee[data-id="' + id + '"]');
+        $selectedBee.remove();
+
+        var $bees = $('.bee');
+        $.map($bees, function (element) {
+            $(element).attr('data-id', (newId++).toString());
+        });
+        
+    }
+
+    var applyAnnimationOnSelectedBee = function (data) {
+        var $selectedBee = $('.bee[data-id="' + data.id + '"]');
+        $selectedBee.css('background-color', 'lightpink');
+
+        $selectedBee.fadeOut(300, function () {
+            $selectedBee.fadeIn(400, function () {
+                $selectedBee.html(data.pointsLeft);
+            });
+        });
+    }
+
+    $('#hitBtn').on('click', function () {
+        $.post('/home/hit', function (data) {
+
+            console.log(data);
+            resetBeesColor();
+
+            if (data.isDead) {
+                onBeeDead(data.id);
+            } else {
+                applyAnnimationOnSelectedBee(data);
+            }
+
+            if (data.isFinish == true) {
+                onFinish();
+            }
+        });
+    });
+});
